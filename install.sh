a430eaaa16d53598b5923f7cd6a9a9ee45d10f4f
bauke molenaar <bauke.molenaar@gmail.com>
2018-03-01 23:25:31 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ CONNFIG(){
 UPDATE=sudo apt-get update && sudo apt-get upgrade -y
 INSTLLDEP=sudo apt-get install git colordiff htop ntop
 }
-bash <(curl -s http://mywebsite.com/myscript.txt)
+bash <(curl -s https://raw.githubusercontent.com/itsdarklikehell/bash_aliases/master/install.sh)
 
 
 CONFIG
