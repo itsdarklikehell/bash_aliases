0e88e9a869fc3fb7526753237db0fd70079792dc
bauke molenaar <bauke.molenaar@gmail.com>
2018-03-01 23:24:17 +0100

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+CONNFIG(){
+UPDATE=sudo apt-get update && sudo apt-get upgrade -y
+INSTLLDEP=sudo apt-get install git colordiff htop ntop
+}
+bash <(curl -s http://mywebsite.com/myscript.txt)
+
+
+CONFIG
+UPDATE
