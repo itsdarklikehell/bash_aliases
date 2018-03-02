0f570a1f9e417e5e9b2e68b39d35f4e1853496e4
bauke molenaar <bauke.molenaar@gmail.com>
2018-03-02 19:08:14 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,9 +5,12 @@ INSTLLDEP=sudo apt-get install git colordiff htop ntop
 }
 CLONE(){
 cd
-git clone https://github.com/itsdarklikehell/bash_aliases/blob/master/.bash_aliases
+git clone https://github.com/itsdarklikehell/bash_aliases
+cd bash_aliases
+cp $HOME/.bash_aliases $HOME/.bash_aliases_old
+cp .bash_aliases $HOM/.bash_aliases
 echo "All done, please source .bash_aliases in all off your consoles (or log out/reboot) to apply changes." 
-source .bash_aliases
+source $HOME/.bash_aliases
 }
 CONFIG
 UPDATE
